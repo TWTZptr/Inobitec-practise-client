fe2986ce49f9535472c8800b7d92268bf7de9a72
TWTZ <twtzzz@yandex.ru>
2021-07-08 18:36:39 +0300

    tree-node

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import './scss/App.scss';
+import './reset.scss';
+import React from 'react';
+import TreeBlock from './components/tree-block';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    return (
+        <div className="container">
+            <TreeBlock/>
+        </div>
+    );
 }
 
 export default App;
